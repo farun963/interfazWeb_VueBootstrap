cf4a39dde01a9799e3abfc64f517a7c2e7a9b993
Reynaldo81 <154021537+Reynaldo81@users.noreply.github.com>
2025-04-06 15:30:08 -0300

    Commit del main

---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+
+
+
+<template>
+    <section class="hero-section text-white text-center py-5" style="background: linear-gradient(to right, #41b883, #34495e);">
+      <div class="container">
+        <h1 class="display-4">Bienvenido a nuestra tienda online</h1>
+        <p class="lead">Encuentra los mejores productos a los mejores precios</p>
+        <button class="btn btn-light btn-lg mt-3">Comprar ahora</button>
+      </div>
+    </section>
+  </template>
+
+  <script setup>
+  </script>
